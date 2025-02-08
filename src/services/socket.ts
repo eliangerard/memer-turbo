2c7184dec23e22afafdafbeb911a89d16d6a5a16
eliangerard <eliangerardxd@gmail.com>
2025-02-07 23:48:29 -0600

    feat: add join and leave functionality for guild rooms in socket service

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -13,6 +13,15 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
-});
 
+  socket.on("join", (guildId) => {
+    console.log("joining room", guildId);
+    socket.join(guildId);
+  });
+
+  socket.on("leave", (guildId) => {
+    console.log("leaving room", guildId);
+    socket.leave(guildId);
+  });
+});
 export { io, server, app };
